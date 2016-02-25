04d2f565222c48bfce66d35b59ce3ef631629cc7
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-02-25 09:44:26 +0200

    Add upload-docs.sh script

---
+++ upload-docs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. env-postgres-osx.sh
+
+set -ex
+
+# Check that we have aws
+aws help > /dev/null
+
+export STACK_YAML=stack-lts-5.yaml
+
+# Generate documentation
+stack haddock
+
+aws s3 --profile docs.futurice.com --region eu-west-1 sync --delete \
+    $(stack path --local-doc-root) \
+    s3://docs.futurice.com/haskell-mega-repo
