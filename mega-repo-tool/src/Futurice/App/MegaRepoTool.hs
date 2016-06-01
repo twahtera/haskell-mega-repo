bb4a20ea7b71269fec1c66382f5fb57230448bf4
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-06-01 15:08:41 +0300

    Add mega-repo-tool build-docker

--- mega-repo-tool/src/Futurice/App/MegaRepoTool.hs
+++ mega-repo-tool/src/Futurice/App/MegaRepoTool.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE NoImplicitPrelude  #-}
 module Futurice.App.MegaRepoTool (defaultMain) where
@@ -8,17 +7,23 @@ import Futurice.Prelude
 import qualified Options.Applicative as O
 
 import Futurice.App.MegaRepoTool.Command.ListSnapshotDependencies
+import Futurice.App.MegaRepoTool.Command.BuildDocker
 
 data Cmd
     = ListSnapshotDependencies 
+    | BuildDocker
     deriving Show
 
 listSnapshotDependenciesOptions :: O.Parser Cmd
 listSnapshotDependenciesOptions = pure ListSnapshotDependencies
 
+buildDockerOptions :: O.Parser Cmd
+buildDockerOptions = pure BuildDocker
+
 optsParser :: O.Parser Cmd
 optsParser = O.subparser $ mconcat
     [ cmdParser "list-snapshot-dependencies" listSnapshotDependenciesOptions "List snapshot dependencies (like stack list-dependencies)"
+    , cmdParser "build-docker" buildDockerOptions "Build docker images"
     ]
   where
     cmdParser :: String -> O.Parser Cmd -> String -> O.Mod O.CommandFields Cmd
@@ -27,9 +32,10 @@ optsParser = O.subparser $ mconcat
 
 main' :: Cmd -> IO ()
 main' ListSnapshotDependencies = listSnapshotDependencies
+main' BuildDocker = buildDocker
 
 defaultMain :: IO ()
-defaultMain = do
+defaultMain =
     O.execParser opts >>= main'
   where
     opts = O.info (O.helper <*> optsParser) $ mconcat
