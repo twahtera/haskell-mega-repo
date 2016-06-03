37b8fe851acd7880f28b7d6b9667e47dfbab3b34
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-06-03 16:42:31 +0300

    Streaming parsing of big array-json

--- futucli/src/Futurice/App/Futucli/Command/FlowdockFormat.hs
+++ futucli/src/Futurice/App/Futucli/Command/FlowdockFormat.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Futurice.App.Futucli.Command.FlowdockFormat (flowdockFormat) where
 
 import Futurice.Prelude
@@ -7,23 +8,56 @@ import Futurice.Prelude
 import Futurice.App.Futucli.Cfg
 
 import Control.Lens      (re, to, _Show)
-import Data.Aeson.Compat (Value, decode)
+import Data.Aeson.Compat (FromJSON, Result (..), Value, fromJSON)
+import Data.Aeson.Parser (value)
 
 import qualified Chat.Flowdock.REST          as FD
 import qualified Chat.Flowdock.REST.Internal as FD
 import qualified Data.ByteString.Lazy        as LBS
 import qualified Data.Text.IO                as T
 
-d :: LBS.ByteString -> IO (Vector FD.Message)
-d = decode
+import qualified Data.Attoparsec.ByteString.Char8 as A8
+import qualified Data.Attoparsec.ByteString.Lazy  as A
 
 flowdockFormat :: FilePath -> Cfg ->  IO ()
 flowdockFormat fp _ = do
     contents <- LBS.readFile fp
-    events <- d contents
+    let (events, err) = streamDecode contents
     traverse_ p events
+    maybe (pure ()) print err
 
 p :: FD.Message -> IO ()
-p msg@FD.Message { FD._msgContent = FD.MTMessage value } =
-    T.putStrLn $ "<" <> msg ^. FD.msgUser . to FD.getIdentifier .  re _Show . packed <>  "> " <> value
+p msg@FD.Message { FD._msgContent = FD.MTMessage contents } =
+    T.putStrLn $ "<" <> msg ^. FD.msgUser . to FD.getIdentifier .  re _Show . packed <>  "> " <> contents
 p _ = pure ()
+
+-------------------------------------------------------------------------------
+-- Streaming parsing of JSON array:
+-------------------------------------------------------------------------------
+
+streamParse :: LBS.ByteString -> ([Value], Maybe String)
+streamParse = start
+  where
+    start bs = case A.parse (A8.char8 '[') bs of
+        A.Done bs' _    -> go bs'
+        A.Fail _ _ err  -> ([], Just err)
+    go bs = case A.parse valueEnd bs of
+        A.Done _   (r, False) -> ([r], Nothing)
+        A.Done bs' (r, True)  -> case go bs' of
+            (rs, end)   -> (r:rs, end)
+        A.Fail _ _ err  -> ([], Just err)
+    valueEnd = do
+        v <- value
+        c <- True <$ A8.char ',' <|> False <$ A8.char ']'
+        return (v, c)
+
+streamDecode :: forall a. FromJSON a => LBS.ByteString -> ([a], Maybe String)
+streamDecode bs = go values
+  where
+    (values, err)  = streamParse bs
+    go :: [Value] -> ([a], Maybe String)
+    go []     = ([], err)
+    go (v:vs) = case fromJSON v of
+        Error err'  -> ([], Just err')
+        Success x  -> case go vs of
+            (xs, err') -> (x:xs, err')
