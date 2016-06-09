467e03f923a9e301578c9fc0298ba69bd7275e9f
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-06-09 13:22:39 +0300

    Basic Auth for proxy-app

--- proxy-app/src/Futurice/App/Proxy/Config.hs
+++ proxy-app/src/Futurice/App/Proxy/Config.hs
@@ -6,11 +6,14 @@ module Futurice.App.Proxy.Config (
 import Futurice.Prelude
 import Prelude          ()
 
+import Data.ByteString    (ByteString)
 import Futurice.EnvConfig
 
 data Config = Config
     { cfgPort             :: !Int
     , cfgFutuhoursBaseurl :: !String
+    , cfgBasicAuthUser    :: !ByteString
+    , cfgBasicAuthPass    :: !ByteString
     }
 
 -- | TODO: parse
@@ -18,3 +21,5 @@ getConfig :: IO Config
 getConfig = Config
     <$> parseDefaultPort
     <*> parseEnvVar "FUTUHOURS_BASEURL"
+    <*> parseEnvVar "PROXYAPP_USER"
+    <*> parseEnvVar "PROXYAPP_PASS"
