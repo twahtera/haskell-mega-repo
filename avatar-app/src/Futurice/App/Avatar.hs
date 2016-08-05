04c71a4b2804fca1ef0ec79f14999c55409c2ede
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-08-05 09:57:45 +0300

    Use futurice-servant in avatar-app

--- avatar-app/src/Futurice/App/Avatar.hs
+++ avatar-app/src/Futurice/App/Avatar.hs
@@ -6,57 +6,53 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE TemplateHaskell #-}
 module Futurice.App.Avatar (defaultMain) where
 
-import Prelude          ()
-import Futurice.Prelude 
+import Futurice.Prelude
+import Prelude ()
 
 import Codec.Picture              (DynamicImage)
 import Control.Monad.Trans.Except (ExceptT (..), throwE)
-import Data.Time                  (getCurrentTime)
-import Development.GitRev         (gitHash)
-import Network.Wai
+import Futurice.Servant
+import Network.Wai                (Application)
 import Servant
-import Servant.Cache.Class        (DynMapCache, cachedIO)
 import System.IO                  (hPutStrLn, stderr)
 
-import qualified Data.ByteString.Lazy          as LBS
-import qualified Data.Text                     as T
-import qualified Data.Text.Encoding            as TE
-import qualified Network.Wai.Handler.Warp      as Warp
-import qualified Servant.Cache.Internal.DynMap as DynMap
+import qualified Data.ByteString.Lazy     as LBS
+import qualified Data.Text                as T
+import qualified Data.Text.Encoding       as TE
+import qualified Network.Wai.Handler.Warp as Warp
 
-import Network.HTTP.Client     (Manager, httpLbs, newManager, parseUrl,
-                                responseBody)
+import Network.HTTP.Client
+       (Manager, httpLbs, newManager, parseUrl, responseBody)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 
 -- Avatar modules
 import Futurice.App.Avatar.API
-import Futurice.App.Avatar.Config  (Config (..), getConfig)
-import Futurice.App.Avatar.Logic   (avatar)
-import Futurice.App.Avatar.Orphans ()
+import Futurice.App.Avatar.Config (Config (..), getConfig)
+import Futurice.App.Avatar.Logic  (avatar)
 
 -- | /TODO:/ We probably will have some
 type Ctx = (DynMapCache, Manager)
 
 type DynamicImage' = Headers '[Header "Cache-Control" Text] DynamicImage
 
-mkAvatar :: Ctx
-         -> Maybe Text  -- ^ URL, is mandatory
-         -> Maybe Int   -- ^ size, minimum size is 16
-         -> Bool        -- ^ greyscale
-         -> ExceptT ServantErr IO DynamicImage'
+mkAvatar
+    :: Ctx
+    -> Maybe Text  -- ^ URL, is mandatory
+    -> Maybe Int   -- ^ size, minimum size is 16
+    -> Bool        -- ^ greyscale
+    -> ExceptT ServantErr IO DynamicImage'
 mkAvatar _ Nothing _ _ =
     throwE $ ServantErr 400 errMsg (fromString errMsg) []
   where
     errMsg = "'url' query parameter is required"
 mkAvatar (cache, mgr) (Just url) msize grey = ExceptT . fmap (first f) $ do
-    hPutStrLn stderr $
-        mconcat [ "fetching ", T.unpack url
-                , " size: ", show msize
-                , " grey: ", show grey
-                ]
+    hPutStrLn stderr $ mconcat
+        [ "fetching ", T.unpack url
+        , " size: ", show msize
+        , " grey: ", show grey
+        ]
     req <- parseUrl (T.unpack url)
     -- XXX: The cache will eventually fill is service is abused
     res <- cachedIO cache 3600 url $ httpLbs req mgr
@@ -66,32 +62,32 @@ mkAvatar (cache, mgr) (Just url) msize grey = ExceptT . fmap (first f) $ do
         $ res
   where
     size  = max 16 $ fromMaybe 32 msize
-    f err = ServantErr 500
-                       "Avatar conversion error"
-                       (LBS.fromStrict . TE.encodeUtf8 . T.pack $ err)
-                       []
+    f err = ServantErr
+        500
+        "Avatar conversion error"
+        (LBS.fromStrict . TE.encodeUtf8 . T.pack $ err)
+        []
 
--- | API server
 server :: Ctx -> Server AvatarAPI
 server ctx = pure "Hello from avatar app"
     :<|> mkAvatar ctx
 
--- | Server with docs and cache and status
-server' :: DynMapCache -> UTCTime -> String -> Ctx -> Server AvatarAPI'
-server' cache start hash' ctx = serverAvatarApi cache start hash' (server ctx)
+server' :: DynMapCache -> Ctx -> Server AvatarAPI'
+server' cache  ctx = futuriceServer
+    "Avatar API"
+    "Serve smaller versions of your favourite images"
+    cache avatarApi (server ctx)
 
--- | Wai application
-app :: DynMapCache -> UTCTime -> String -> Ctx -> Application
-app cache start hash' ctx = serve avatarApi' (server' cache  start hash' ctx)
+app :: DynMapCache -> Ctx -> Application
+app cache ctx = serve avatarApi' (server' cache ctx)
 
 defaultMain :: IO ()
 defaultMain = do
     hPutStrLn stderr "Hello, avatar-app is alive"
     Config{..} <- getConfig
     mgr <- newManager tlsManagerSettings
-    cache <- DynMap.newIO
-    now <- getCurrentTime
+    cache <- newDynMapCache
     let ctx = (cache, mgr)
-    let app' = app cache now $(gitHash) ctx
+    let app' = app cache ctx
     hPutStrLn stderr "Starting web server"
     Warp.run cfgPort app'
--- avatar-app/src/Futurice/App/Avatar/API.hs
+++ avatar-app/src/Futurice/App/Avatar/API.hs
@@ -5,22 +5,15 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Futurice.App.Avatar.API where
 
-import Prelude        ()
 import Futurice.Prelude
+import Prelude ()
 
-import Codec.Picture   (DynamicImage)
-import Futurice.Colour
-
+import Codec.Picture       (DynamicImage)
+import Futurice.Servant
 import Servant
-import Servant.Cache.Class    (DynMapCache)
-import Servant.Docs
-import Servant.Futurice.Utils
-import Servant.JuicyPixels    (PNG)
-
-import Futurice.App.Avatar.Orphans ()
+import Servant.JuicyPixels (PNG)
 
 type AvatarAPI =
     Get '[PlainText] Text
@@ -29,34 +22,10 @@ type AvatarAPI =
                   :> QueryFlag "grey"
                   :> Get '[PNG] (Headers '[Header "Cache-Control" Text] DynamicImage)
 
-type AvatarAPI' = AvatarAPI :<|> AuxAPI ('FutuAccent 'AF5 'AC2)
+type AvatarAPI' = FuturiceAPI AvatarAPI  ('FutuAccent 'AF5 'AC2)
 
 avatarApi :: Proxy AvatarAPI
 avatarApi = Proxy
 
 avatarApi' :: Proxy AvatarAPI'
 avatarApi' = Proxy
-
-serverAvatarApi :: DynMapCache -> UTCTime -> String -> Server AvatarAPI -> Server AvatarAPI'
-serverAvatarApi cache start now server = server :<|> serverAuxApi cache start now avatarApi mempty
-
-instance ToParam (QueryParam "url" Text) where
-  toParam _ =
-    DocQueryParam "url"
-                  ["example.org/com"]
-                  "The url of the image"
-                  Normal
-
-instance ToParam (QueryParam "size" Int) where
-  toParam _ =
-    DocQueryParam "size"
-                  ["32"]
-                  "The size of resulting avatar"
-                  Normal
-
-instance ToParam (QueryFlag "grey") where
-  toParam _ =
-    DocQueryParam "grey"
-                  ["true", "false"]
-                  "The greyscale image"
-                 Normal
