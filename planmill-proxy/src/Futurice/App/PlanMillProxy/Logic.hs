31870f9cbe4c795dd58e843c09b53e72e01b1dd0
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-08-08 14:46:16 +0300

    [planmill-proxy] store results in postgres

--- planmill-proxy/src/Futurice/App/PlanMillProxy/Logic.hs
+++ planmill-proxy/src/Futurice/App/PlanMillProxy/Logic.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Futurice.App.PlanMillProxy.Logic (
     haxlEndpoint,
     ) where
@@ -6,44 +7,79 @@ module Futurice.App.PlanMillProxy.Logic (
 import Futurice.Prelude
 import Prelude ()
 
+import Control.Monad.Catch              (handle)
 import Control.Monad.PlanMill           (planmillQuery)
 import Data.Binary.Tagged
        (HasSemanticVersion, HasStructuralInfo, taggedEncode)
 import Data.ByteString.Lazy             (ByteString)
 import Data.Constraint
+import Data.Pool                        (withResource)
 import Futurice.App.PlanMillProxy.H
 import Futurice.App.PlanMillProxy.Types (Ctx (..))
 import Futurice.Servant                 (cachedIO)
 import PlanMill.Types                   (Cfg)
 import PlanMill.Types.Query             (Query, SomeQuery (..), queryDict)
 
+import qualified Data.Aeson                 as Aeson
+import qualified Database.PostgreSQL.Simple as Postgres
+
 haxlEndpoint :: Ctx -> [SomeQuery] -> IO [Either Text ByteString]
-haxlEndpoint ctx = traverse fetch
+haxlEndpoint ctx qs = withResource (ctxPostgresPool ctx) $ \conn ->
+    traverse (fetch conn) qs
   where
-    fetch :: SomeQuery -> IO (Either Text ByteString)
-    fetch (SomeQuery q) = case (binaryDict, semVerDict, structDict) of
-        (Dict, Dict, Dict) -> taggedEncode <$$> fetch' q
+    fetch :: Postgres.Connection -> SomeQuery -> IO (Either Text ByteString)
+    fetch conn (SomeQuery q) =
+        case (binaryDict, semVerDict, structDict, nfdataDict, typeableDict) of
+            (Dict, Dict, Dict, Dict, Dict) -> taggedEncode <$$> fetch' conn q
       where
         binaryDict = queryDict (Proxy :: Proxy Binary) (Sub Dict) q
         semVerDict = queryDict (Proxy :: Proxy HasSemanticVersion) (Sub Dict) q
         structDict = queryDict (Proxy :: Proxy HasStructuralInfo) (Sub Dict) q
-
-    fetch' :: Query a -> IO (Either Text a)
-    fetch' q = case (nfdataDict, typeableDict) of
-        (Dict, Dict) -> do
-            res <- tryDeep $ fetchCached ctx q
-            return $ first (\x -> show x ^. packed) res
-      where
         nfdataDict = queryDict (Proxy :: Proxy NFData) (Sub Dict) q
         typeableDict = queryDict (Proxy :: Proxy Typeable) (Sub Dict) q
 
+    fetch'
+        :: (NFData a, Typeable a, Binary a, HasSemanticVersion a, HasStructuralInfo a)
+        => Postgres.Connection -> Query a -> IO (Either Text a)
+    fetch' conn q = do
+        res <- tryDeep $ fetchCached ctx conn q
+        return $ first (\x -> show x ^. packed) res
+
 -- | Run query trhu cache.
-fetchCached :: Typeable a => Ctx -> Query a -> IO a
-fetchCached ctx q = cachedIO cache 3600 q $ fetchFromPlanMill cfg q
+fetchCached
+    :: forall a. (Typeable a, Binary a, HasSemanticVersion a, HasStructuralInfo a)
+    => Ctx -> Postgres.Connection -> Query a -> IO a
+fetchCached ctx conn q = cachedIO cache 3600 q $ do
+    x <- fetchFromPlanMill cfg q
+    storeInPostgres x
+    pure x
   where
     cache = ctxCache ctx
     cfg   = ctxPlanmillCfg ctx
+    aesonQuery = Aeson.encode q
+
+    storeInPostgres :: a -> IO ()
+    storeInPostgres x = do
+        i <- handle (omitSqlError 0) $
+            Postgres.execute conn postgresQuery (aesonQuery, b)
+        print i -- log!
+      where
+        postgresQuery = fromString $ unwords $
+            [ "INSERT INTO planmillproxy.cache as c (query, data)"
+            , "VALUES (?, ?)"
+            , "ON CONFLICT (query) DO UPDATE"
+            , "SET data = EXCLUDED.data, viewed = 0, updated = now()"
+            , "WHERE c.query = EXCLUDED.query;"
+            ]
+
+        b =  taggedEncode x
 
 -- | Run query on real planmill backend.
 fetchFromPlanMill :: Cfg -> Query a -> IO a
 fetchFromPlanMill cfg q = runH cfg $ planmillQuery q
+
+omitSqlError :: a -> Postgres.SqlError -> IO a
+omitSqlError a err = do
+    print err
+    -- $(logError) $ textShow err
+    return a
