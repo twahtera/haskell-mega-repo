27bd59fa40b4d7e510a5fafe3a59f317d83b1f63
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-08-08 13:25:12 +0300

    [planmill-proxy] Refactor current cache implementation

--- planmill-proxy/src/Futurice/App/PlanMillProxy/Logic.hs
+++ planmill-proxy/src/Futurice/App/PlanMillProxy/Logic.hs
@@ -7,19 +7,21 @@ import Futurice.Prelude
 import Prelude ()
 
 import Control.Monad.PlanMill           (planmillQuery)
-import Data.Binary.Tagged               (taggedEncode, HasSemanticVersion, HasStructuralInfo)
+import Data.Binary.Tagged
+       (HasSemanticVersion, HasStructuralInfo, taggedEncode)
 import Data.ByteString.Lazy             (ByteString)
 import Data.Constraint
 import Futurice.App.PlanMillProxy.H
 import Futurice.App.PlanMillProxy.Types (Ctx (..))
 import Futurice.Servant                 (cachedIO)
+import PlanMill.Types                   (Cfg)
 import PlanMill.Types.Query             (Query, SomeQuery (..), queryDict)
 
 haxlEndpoint :: Ctx -> [SomeQuery] -> IO [Either Text ByteString]
 haxlEndpoint ctx = traverse fetch
   where
     fetch :: SomeQuery -> IO (Either Text ByteString)
-    fetch (SomeQuery q) = case (binaryDict, semVerDict, structDict) of 
+    fetch (SomeQuery q) = case (binaryDict, semVerDict, structDict) of
         (Dict, Dict, Dict) -> taggedEncode <$$> fetch' q
       where
         binaryDict = queryDict (Proxy :: Proxy Binary) (Sub Dict) q
@@ -29,11 +31,19 @@ haxlEndpoint ctx = traverse fetch
     fetch' :: Query a -> IO (Either Text a)
     fetch' q = case (nfdataDict, typeableDict) of
         (Dict, Dict) -> do
-            res <- tryDeep $ cachedIO cache 3600 q $ runH cfg $ planmillQuery q
+            res <- tryDeep $ fetchCached ctx q
             return $ first (\x -> show x ^. packed) res
       where
         nfdataDict = queryDict (Proxy :: Proxy NFData) (Sub Dict) q
         typeableDict = queryDict (Proxy :: Proxy Typeable) (Sub Dict) q
 
+-- | Run query trhu cache.
+fetchCached :: Typeable a => Ctx -> Query a -> IO a
+fetchCached ctx q = cachedIO cache 3600 q $ fetchFromPlanMill cfg q
+  where
     cache = ctxCache ctx
     cfg   = ctxPlanmillCfg ctx
+
+-- | Run query on real planmill backend.
+fetchFromPlanMill :: Cfg -> Query a -> IO a
+fetchFromPlanMill cfg q = runH cfg $ planmillQuery q
