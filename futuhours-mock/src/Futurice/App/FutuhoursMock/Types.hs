a4d9e16a56f6a9fc8f45436a01badcf404b06200
Jussi Vaihia <jussiava@gmail.com>
2016-09-22 16:31:23 +0300

    Task, Entry

--- futuhours-mock/src/Futurice/App/FutuhoursMock/Types.hs
+++ futuhours-mock/src/Futurice/App/FutuhoursMock/Types.hs
@@ -18,6 +18,7 @@ module Futurice.App.FutuhoursMock.Types (
 import Prelude ()
 import Futurice.Prelude
 import Futurice.Generics
+import Data.Time (UTCTime, diffUTCTime, getCurrentTime)
 
 import qualified PlanMill as PM
 
@@ -28,13 +29,40 @@ import qualified PlanMill as PM
 data Project = Project
     { _projectId     :: PM.ProjectId
     , _projectName   :: !Text
---  ,  tasks     :: [Task]
+    ,  tasks     :: [Task]
     , _projectClosed :: !Bool
     }
   deriving (Eq, Show, Typeable, Generic)
-  
+
+data Task = Task
+  { _taskId :: PM.TaskId
+  , _taskName :: !Text
+  , _taskAbsence :: !Bool
+  , _taskClosed :: !Bool
+  , _taskLatestEntry :: Maybe Entry
+  , _taskHoursRemaining :: !Float
+  } deriving (Eq, Show, Typeable, Generic)
+-- LatestEntry: Hours UI feature. Previous Entry used as default values when marking new hours.
+-- HoursRemaing: Task.taskTargetEffort - (totalHoursUsedByAssignedPersonnel)
+
+data Entry = Entry
+  { _entryId :: PM.TimereportId
+  , _entryProjectId :: PM.ProjectId
+  , _entrytaskId :: PM.TaskId
+  , _latestEntryDate :: !UTCTime
+  , _entryDescription :: !Text
+  , _entryClosed :: !Bool
+  , _entryhours :: !Float
+  } deriving (Eq, Show, Typeable, Generic)
+
 makeLenses ''Project
 deriveGeneric ''Project
+
+makeLenses ''Task
+deriveGeneric ''Task
+
+makeLenses ''Entry
+deriveGeneric ''Entry
 
 instance Arbitrary Project where
     arbitrary = sopArbitrary
@@ -43,6 +71,22 @@ instance Arbitrary Project where
 instance ToJSON Project where toJSON = sopToJSON
 instance FromJSON Project where parseJSON = sopParseJSON
 instance ToSchema Project where declareNamedSchema = sopDeclareNamedSchema
+
+instance Arbitrary Task where
+    arbitrary = sopArbitrary
+    shrink    = sopShrink
+
+instance ToJSON Task where toJSON = sopToJSON
+instance FromJSON Task where parseJSON = sopParseJSON
+instance ToSchema Task where declareNamedSchema = sopDeclareNamedSchema
+
+instance Arbitrary Entry where
+    arbitrary = sopArbitrary
+    shrink    = sopShrink
+
+instance ToJSON Entry where toJSON = sopToJSON
+instance FromJSON Entry where parseJSON = sopParseJSON
+instance ToSchema Entry where declareNamedSchema = sopDeclareNamedSchema
 
 -------------------------------------------------------------------------------
 -- Context
