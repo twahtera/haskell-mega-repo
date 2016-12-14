ba2ffd6088ab685266066ded2c8efe157cf7672c
Jussi Vaihia <jussiava@gmail.com>
2016-12-14 11:06:06 +0200

    fix edits on wrong file

--- avatar-app/src/Futurice/App/Avatar.hs
+++ avatar-app/src/Futurice/App/Avatar.hs
@@ -75,6 +75,6 @@ defaultMain = futuriceServerMain makeCtx $ emptyServerConfig
     & serverEnvPfx        .~ "AVATAR"
   where
     makeCtx :: Config -> Logger -> DynMapCache -> IO Ctx
-    makeCtx cfg _logger cache = do
+    makeCtx _cfg _logger cache = do
         mgr <- newManager tlsManagerSettings
         return (cache, mgr)
