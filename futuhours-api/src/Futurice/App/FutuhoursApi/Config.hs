9079f8b780db6767f3c4bd964f60047e5d985725
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-12-18 01:48:12 +0200

    Add basic config to futuhours-api-server

--- futuhours-api/src/Futurice/App/FutuhoursApi/Config.hs
+++ futuhours-api/src/Futurice/App/FutuhoursApi/Config.hs
@@ -6,7 +6,27 @@ import Prelude ()
 import Futurice.Prelude
 import Futurice.EnvConfig
 
+import qualified FUM
+import qualified PlanMill as PM
+
 data Config = Config
+    { cfgPlanmillUrl       :: !String
+      -- ^ Planmill url
+    , cfgPlanmillAdminUser :: !PM.UserId
+      -- ^ Admin user id
+    , cfgPlanmillSignature :: !PM.ApiKey
+      -- ^ Token
+    , cfgFumToken          :: !FUM.AuthToken
+    , cfgFumBaseurl        :: !FUM.BaseUrl
+    , cfgFumList           :: !FUM.ListName
+    }
+  deriving (Show)
 
 instance Configure Config where
-    configure = pure Config
+    configure = Config
+        <$> envVar "PLANMILL_BASEURL"
+        <*> envVar "PLANMILL_ADMIN"
+        <*> envVar "PLANMILL_SIGNATURE"
+        <*> envVar "FUM_TOKEN"
+        <*> envVar "FUM_BASEURL"
+        <*> envVar "FUM_LISTNAME"
