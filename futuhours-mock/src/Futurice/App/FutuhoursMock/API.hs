c038f28da1146a1e3f9b8b9288c99fa34780b9f9
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-12-17 20:12:15 +0200

    Reformat FutuhoursMock.API

--- futuhours-mock/src/Futurice/App/FutuhoursMock/API.hs
+++ futuhours-mock/src/Futurice/App/FutuhoursMock/API.hs
@@ -7,22 +7,18 @@
 {-# LANGUAGE TypeOperators         #-}
 module Futurice.App.FutuhoursMock.API where
 
-import Futurice.Prelude
 import Prelude ()
-
+import Futurice.Prelude
 import Futurice.App.FutuhoursMock.Types
 import Servant
 
 type FutuhoursV1API =
-      ( "projects" :> Get '[JSON] (Vector Project)
-        :<|> "user" :> Get '[JSON] (User) -- TODO: should return logged-in User information
-        :<|> "hours" :> QueryParam "start-date" Day
-                     :> QueryParam "end-date" Day
-                     :> Get '[JSON] (HoursResponse)
-        :<|> "entry" :> ReqBody '[JSON] EntryUpdate :> Post '[JSON] (EntryUpdateResponse)
-        :<|> "entry" :> Capture "id" Int :> ReqBody '[JSON] EntryUpdate :> Put '[JSON] (EntryUpdateResponse)
-        :<|> "entry" :> Capture "id" Int :> ReqBody '[JSON] EntryUpdate :> Delete '[JSON] (EntryUpdateResponse)
-      )
+    "projects" :> Get '[JSON] (Vector Project)
+    :<|> "user" :> Get '[JSON] User -- TODO: should return logged-in User information
+    :<|> "hours" :> QueryParam "start-date" Day :> QueryParam "end-date" Day :> Get '[JSON] (HoursResponse)
+    :<|> "entry" :> ReqBody '[JSON] EntryUpdate :> Post '[JSON] EntryUpdateResponse
+    :<|> "entry" :> Capture "id" Int :> ReqBody '[JSON] EntryUpdate :> Put '[JSON] EntryUpdateResponse
+    :<|> "entry" :> Capture "id" Int :> ReqBody '[JSON] EntryUpdate :> Delete '[JSON] EntryUpdateResponse
 
 type FutuhoursAPI = Get '[JSON] Text
     :<|> "api" :> "v1" :> FutuhoursV1API
