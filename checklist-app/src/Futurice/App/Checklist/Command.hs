62c2b0d20d5d8174ad71a11e3fa4d1e01edf0a77
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-01-03 10:28:45 +0200

    Proper CmdCreateEmployee handling

--- checklist-app/src/Futurice/App/Checklist/Command.hs
+++ checklist-app/src/Futurice/App/Checklist/Command.hs
@@ -29,8 +29,9 @@ module Futurice.App.Checklist.Command (
 
 import Prelude ()
 import Futurice.Prelude
-import Control.Lens      (set, (%~))
-import Data.Swagger      (NamedSchema (..))
+import Control.Lens               (iforOf_, non, (%=), (?=))
+import Control.Monad.State.Strict (execState)
+import Data.Swagger               (NamedSchema (..))
 import Futurice.Aeson
        (FromJSONField1, fromJSONField1, object, withObject, (.!=), (.:), (.:?),
        (.=))
@@ -225,26 +226,37 @@ traverseCommand _f (CmdRemoveTask c t) =
 traverseCommand f (CmdCreateEmployee e c x) =
     CmdCreateEmployee <$> f e <*> pure c <*> pure x
 
+-- = operators are the same as ~ lens operators, but modify the state of MonadState.
+--
 -- todo: in error monad, if e.g. identifier don't exist
 applyCommand :: Command Identity -> World -> World
-applyCommand cmd world = case cmd of
+applyCommand cmd world = flip execState world $ case cmd of
     CmdCreateChecklist (Identity cid) n ->
-        world & worldLists . at cid ?~ Checklist cid n mempty
+        worldLists . at cid ?= Checklist cid n mempty
+
     CmdCreateTask (Identity tid) (TaskEdit (Identity n) (Identity role)) ->
-        world & worldTasks . at tid ?~ Task tid n mempty role
+        worldTasks . at tid ?= Task tid n mempty role
+
     CmdAddTask cid tid app ->
-        world & worldLists . ix cid . checklistTasks . at tid ?~ app
+        worldLists . ix cid . checklistTasks . at tid ?= app
+        -- TODO: add to worldTaskItems
+
     CmdRemoveTask cid tid ->
-        world & worldLists . ix cid . checklistTasks . at tid .~ Nothing
+        worldLists . ix cid . checklistTasks . at tid Lens..= Nothing
+
     CmdRenameChecklist cid n ->
-        world & worldLists . ix cid . checklistName .~ n
-    CmdEditTask tid (TaskEdit mn mr) -> world & worldTasks . ix tid %~ update
-      where
-        update task = task
-            & maybe id (set taskName) mn
-            & maybe id (set taskRole) mr
-    CmdCreateEmployee (Identity eid) cid x ->
-        world & worldEmployees . at eid ?~ fromEmployeeEdit eid cid x
+         worldLists . ix cid . checklistName Lens..= n
+
+    CmdEditTask tid te ->
+        worldTasks . ix tid %= applyTaskEdit te
+
+    CmdCreateEmployee (Identity eid) cid x -> do
+        -- create user
+        worldEmployees . at eid ?= fromEmployeeEdit eid cid x
+        -- add initial tasks
+        iforOf_ (worldLists . ix cid . checklistTasks . ifolded) world $ \tid _app ->
+            -- TODO: check appliance before adding
+            worldTaskItems . at eid . non mempty . at tid ?= TaskItemTodo
 
 transactCommand
     :: (MonadLog m, MonadIO m)
