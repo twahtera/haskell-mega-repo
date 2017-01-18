0472289ea117859f2a8db1725aca1719e1df8eba
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-01-18 16:09:30 +0200

    CreateTask: add to checklist at creation - form part

--- checklist-app/src/Futurice/App/Checklist/Pages/CreateTask.hs
+++ checklist-app/src/Futurice/App/Checklist/Pages/CreateTask.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Futurice.App.Checklist.Pages.CreateTask (createTaskPage) where
 
 import Prelude ()
 import Futurice.Prelude
-import Control.Lens              (re)
+import Control.Lens              (re, to)
 import Futurice.Lucid.Foundation
 
 import Futurice.App.Checklist.Markup
@@ -14,7 +15,7 @@ createTaskPage
     :: World
     -> AuthUser    -- ^ logged in user
     -> HtmlPage "create-task"
-createTaskPage _world authUser = checklistPage_ ("Create task") authUser $ do
+createTaskPage world authUser = checklistPage_ ("Create task") authUser $ do
     -- Title
     header "Create task" []
 
@@ -33,7 +34,33 @@ createTaskPage _world authUser = checklistPage_ ("Create task") authUser $ do
                     optionSelected_ (role == r)
                         [ value_ $ role ^. re _TaskRole ]
                         $ toHtml $ role ^. re _TaskRole
+        row_ $ do
+            large_ 12 $ label_ $ do
+                "Checklist 1"
+                checklistSelect "task-checklist-1"
+            --large_ 6 $ label_ $ do
+            --    "Appliance, not implemented"
+        row_ $ do
+            large_ 12 $ label_ $ do
+                "Checklist 2"
+                checklistSelect "task-checklist-2"
+            --large_ 6 $ label_ $ do
+            --    "Appliance, not implemented"
+        row_ $ do
+            large_ 12 $ label_ $ do
+                "Checklist 3"
+                checklistSelect "task-checklist-3"
+            --large_ 6 $ label_ $ do
+            --    "Appliance, not implemented"
 
         row_ $ large_ 12 $ div_ [ class_ "button-group" ] $ do
             button_ [ class_ "button success", data_ "futu-action" "submit" ] $ "Create"
             button_ [ class_ "button", data_ "futu-action" "reset" ] $ "Reset"
+  where
+    checklistSelect :: Monad m => Text -> HtmlT m ()
+    checklistSelect n = select_ [ name_ n ] $ do
+        option_ [ value_ "" ] $ "-"
+        for_ (world ^.. worldLists . folded) $ \cl ->
+            optionSelected_ False
+                [ value_ $ cl ^. identifier . to identifierToText ]
+                $ cl ^. nameHtml
