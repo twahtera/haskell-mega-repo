bdb1494f5f8d990372932da6dc9612010f41cb23
Tuukka Wahtera <tuukka.wahtera@futurice.com>
2017-02-06 11:53:43 +0200

    Use Clay for css in Foundation

---
+++ futurice-foundation/src/Futurice/Lucid/Style.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings      #-}
+module Futurice.Lucid.Style where
+
+import Prelude hiding (div, span)
+import Clay
+
+css :: Css
+css = do
+  ".emphasize" & td ?
+    do fontWeight bold
+       background ("#eee" :: Color)
+  ".empasize2" & td ?
+    do fontStyle italic
+       background ("#efe" :: Color)
+  h1 ? h2 ? h3 ? h4? li ? td ? div ? span ? b ?
+    do fontFamily ["Lucida Grande", "Helvetica", "Arial"] [sansSerif]
+  star ? fontSize (pt 11)
+  h1 ? do fontSize (pt 20)
+          fontWeight bold
+  h2 ? do fontSize (pt 15)
+          fontWeight bold
+  h3 ? do fontSize (pt 13)
+          fontWeight bold
