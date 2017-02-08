200834292d01282ec75ac5e53fafed8e7801c6bd
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-02-08 09:00:28 +0200

    fixedWork can be double

--- planmill-client/src/PlanMill/Types/Project.hs
+++ planmill-client/src/PlanMill/Types/Project.hs
@@ -32,7 +32,7 @@ data Project = Project
     , pCategory                   :: !(Maybe Int)
     , pFinish                     :: !(Maybe UTCTime)
     , pFixedRevenue               :: !(Maybe Double)
-    , pFixedWork                  :: !(Maybe Int)
+    -- , pFixedWork                  :: !(Maybe Double)
     , pFixedWorkEffort            :: !(Maybe Int)
     , pInvoiceAppendix            :: !(Maybe Int)
     , pInvoicedRevenue            :: !(Maybe Double) -- TODO: better type
@@ -80,7 +80,7 @@ instance FromJSON Project where
                 <*> obj .: "category"
                 <*> (getU <$$> obj .:? "finish")
                 <*> obj .: "fixedRevenue"
-                <*> obj .: "fixedWork"
+                -- <*> obj .: "fixedWork"
                 <*> obj .: "fixedWorkEffort"
                 <*> obj .:? "invoiceAppendix"
                 <*> obj .:? "invoicedRevenue"
