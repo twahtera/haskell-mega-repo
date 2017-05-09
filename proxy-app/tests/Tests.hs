dcc319043dece58d4d11e76e0f5dde8063a58d44
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-05-09 12:14:30 +0300

    Add more tags

--- proxy-app/tests/Tests.hs
+++ proxy-app/tests/Tests.hs
@@ -10,6 +10,7 @@ import Test.Tasty
 import Test.Tasty.QuickCheck
 
 import qualified Futurice.GitHub      as GH
+import qualified PlanMill.Types       as PM
 import qualified PlanMill.Types.Query as PM
 import qualified Data.ByteString.Base16.Lazy as Base16
 
@@ -41,6 +42,14 @@ binaryTagTests = testGroup "BinaryTagged tags" $ map mk tags
     tags =
         [ BTTest "PlanMill.SomeResponse" (Proxy :: Proxy PM.SomeResponse)
             0 "6b3e878ac2290517d97df609f908d62a71746646"
+        , BTTest "planmill-haxl endpoint" (Proxy :: Proxy [Either Text PM.SomeResponse])
+            0 "326db708837ebee1bcf07d52622f3dd23533a43f"
+        , BTTest "PlanMill.Projects" (Proxy :: Proxy PM.Projects)
+            0 "23a7e05940eb50f2b32177a4d66aab0bae4dad80"
+        , BTTest "PlanMill.Tasks" (Proxy :: Proxy PM.Tasks)
+            0 "6f624274e2fdac20a4581177c66e194b460169a1"
+        , BTTest "PlanMill.CapacityCalendars" (Proxy :: Proxy PM.CapacityCalendars)
+            0 "1fde910b8a5fc395de41cf0cda34f17fe9d141cd"
         , BTTest "GitHub.SomeResponse" (Proxy :: Proxy GH.SomeResponse)
             0 "e299b9ec4d641300444bd32e17148546c036a3a8"
         ]
