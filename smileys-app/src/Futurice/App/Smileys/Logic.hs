42a5e2f85d422000dd3e8e6d052d586c070434ab
Jussi Vaihia <jussiava@gmail.com>
2017-05-30 16:13:13 +0300

    2to4 spaces

--- smileys-app/src/Futurice/App/Smileys/Logic.hs
+++ smileys-app/src/Futurice/App/Smileys/Logic.hs
@@ -4,7 +4,7 @@ module Futurice.App.Smileys.Logic (getSmileys, postSmiley) where
 
 import Prelude ()
 import Futurice.Prelude
-import Data.Pool                    (withResource)
+import Data.Pool        (withResource)
 
 import Futurice.App.Smileys.Types
 import Futurice.App.Smileys.Ctx
@@ -21,16 +21,16 @@ getSmileys
     -> Maybe Day
     -> m [Smileys]
 getSmileys ctx _ start end = do
-  now <- liftIO $ currentTime
-  let today = utctDay now
-  withResource (ctxPostgresPool ctx) $ \conn -> do
-    res <- q conn (fromMaybe today start) (fromMaybe today end)
-    return $ res
-    where
-      q :: (MonadIO m, MonadBaseControl IO m) => Postgres.Connection -> Day -> Day -> m [Smileys]
-      q conn s e = liftIO $ Postgres.query conn
-              "SELECT entries, username, smiley, day FROM smileys.trail WHERE day >= ? AND day <= ?"
-              (s, e)
+    now <- liftIO $ currentTime
+    let today = utctDay now
+    withResource (ctxPostgresPool ctx) $ \conn -> do
+        res <- q conn (fromMaybe today start) (fromMaybe today end)
+        return $ res
+        where
+            q :: (MonadIO m, MonadBaseControl IO m) => Postgres.Connection -> Day -> Day -> m [Smileys]
+            q conn s e = liftIO $ Postgres.query conn
+                    "SELECT entries, username, smiley, day FROM smileys.trail WHERE day >= ? AND day <= ?"
+                    (s, e)
 
 postSmiley
     :: (MonadIO m, MonadBaseControl IO m, MonadError ServantErr m)
@@ -39,18 +39,18 @@ postSmiley
     -> PostSmiley
     -> m Res
 postSmiley ctx mfum req = do
-  withResource (ctxPostgresPool ctx) $ \conn -> do
-    mcase (mfum <|> ctxMockUser ctx) (throwError err403) $ \fumUsername -> do
-        let insertQuery = fromString $ unwords $
-               [ "INSERT INTO smileys.trail as c (entries, username, smiley, day)"
-               , "VALUES (?, ?, ?, ?) ON CONFLICT (username, day) DO UPDATE"
-               , "SET entries = EXCLUDED.entries, smiley = EXCLUDED.smiley"
-               ]
-        let smiley_entries = _postSmileyEntries req
-        let smiley_day = textShow $ _postSmileyDate req
-        let smiley_user = FUM._getUserName fumUsername
-        let smiley_state = textShow $ _postSmileySmiley req
-        _ <- liftIO $ Postgres.execute conn
-               insertQuery
-               ( smiley_entries, smiley_user, smiley_state, smiley_day)
-        pure $ Res { _resStatus = "OK" }
+    withResource (ctxPostgresPool ctx) $ \conn -> do
+        mcase (mfum <|> ctxMockUser ctx) (throwError err403) $ \fumUsername -> do
+                let insertQuery = fromString $ unwords $
+                     [ "INSERT INTO smileys.trail as c (entries, username, smiley, day)"
+                     , "VALUES (?, ?, ?, ?) ON CONFLICT (username, day) DO UPDATE"
+                     , "SET entries = EXCLUDED.entries, smiley = EXCLUDED.smiley"
+                     ]
+                let smiley_entries = _postSmileyEntries req
+                let smiley_day = textShow $ _postSmileyDate req
+                let smiley_user = FUM._getUserName fumUsername
+                let smiley_state = textShow $ _postSmileySmiley req
+                _ <- liftIO $ Postgres.execute conn
+                         insertQuery
+                         ( smiley_entries, smiley_user, smiley_state, smiley_day)
+                pure $ Res { _resStatus = "OK" }
